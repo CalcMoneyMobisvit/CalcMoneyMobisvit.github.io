af18ee7aa8ab1128e7f46c5573fae18c0745c4f4
CalcMoneyMobisvit <calcmoneymobisvit@gmail.com>
2020-08-16 15:06:15 +0200

    first commit

---
+++ src/components/operation/operation.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Operation = ({ addTransaсtion, addDescription, addAmount, description, amount }) => {
+   return (
+
+      <section className="operation">
+         <h3>Новая операция</h3>
+         <form id="form">
+            <label>
+               <input
+                  type="text"
+                  className="operation__fields operation__name"
+                  placeholder="Наименование операции"
+                  onChange={addDescription}
+                  value={description}
+               />
+
+            </label>
+            <label>
+               <input
+                  type="number"
+                  className="operation__fields operation__amount"
+                  placeholder="Введите сумму"
+                  onChange={addAmount}
+                  value={amount}
+               />
+
+            </label>
+            <div className="operation__btns">
+               <button
+                  onClick={() => addTransaсtion(false)}
+                  type="button"
+                  className="operation__btn operation__btn-subtract"
+               > РАСХОД</button>
+               <button
+                  onClick={() => addTransaсtion(true)}
+                  type="button"
+                  className="operation__btn operation__btn-add"
+               >ДОХОД</button>
+            </div>
+         </form>
+      </section>
+
+   )
+}
+
+export default Operation;
